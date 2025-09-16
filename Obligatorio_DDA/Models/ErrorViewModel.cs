5c3924c27e30991e510794ff2f052bb822b5506a
AleRabazza <alejandrarabazza@gmail.com>
2025-09-15 21:15:35 -0300

    Agregar archivos de proyecto.

---
+++ Obligatorio_DDA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Obligatorio_DDA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
